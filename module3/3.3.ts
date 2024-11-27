d30286ee853282be7b8d35128a76d67904ba1e01
mdshahadad <identity.shahadad@gmail.com>
2024-11-28 00:00:20 +0600

    Learn type guard with in gurad

--- module3/3.3.ts
+++ module3/3.3.ts
@@ -15,4 +15,39 @@
     const result1 = add(2, 3);
     console.log(result1);
 
+
+    // Type Guard using in guard
+
+    type NormalUser = {
+        name: string;
+    }
+
+    type AdminUser = {
+        name: string;
+        role: 'admin';
+    }
+
+
+    const getUser = (user: NormalUser | AdminUser) => {
+        if ('role' in user) {
+            console.log(`${user.name} is an ${user.role}`);
+        } else {
+            console.log(`${user.name} is a normal user`);
+        }
+    };
+
+    const normalUser: NormalUser = {
+        name: 'Mr Normal',
+    }
+
+    const adminUser: AdminUser = {
+        name: 'Mr Admin',
+        role: 'admin',
+    }
+
+    getUser(normalUser);
+
+
+
+
 }
