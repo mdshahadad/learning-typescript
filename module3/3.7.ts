faf811ae74ea1edc172fd125ecaacaad1eff82f7
mdshahadad <identity.shahadad@gmail.com>
2024-11-28 02:09:49 +0600

    Learn static modifire

---
+++ module3/3.7.ts
@@ -0,0 +1,23 @@
+{
+
+    // Static in oop
+    class Counter {
+        static count: number = 0;
+
+        static increment() {
+            return (Counter.count = Counter.count + 1);
+        };
+
+        static decrement() {
+            return (Counter.count - 1);
+        };
+    };
+
+    console.log(Counter.increment());
+    console.log(Counter.increment());
+    console.log(Counter.increment());
+    console.log(Counter.increment());
+    console.log(Counter.decrement());
+
+
+}
